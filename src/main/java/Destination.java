e972389cad15ab2a647f419c399e10764fa55c9f
eszterkaloz <eszterkaloz@gmail.com>
2017-03-21 00:59:36 +0100

    Implement logic for graph search

--- src/main/java/Destination.java
+++ src/main/java/Destination.java
@@ -7,9 +7,31 @@ public class Destination {
     private String name;
     private List<Destination> dependencies;
     private boolean visited;
+    private List<String> destinationNames = new ArrayList<>();
 
     public Destination(String name) {
         this.name = name;
+    }
+
+    public List<String> planDestination(Destination... destinations) {
+        for(Destination destination : destinations) {
+            order(destination);
+        }
+        return destinationNames;
+    }
+
+    private void order(Destination destination) {
+        if (destination.isVisited()) {
+            return;
+        }
+
+        destination.setVisited(true);
+        if(destination.getDependencies() != null) {
+            for (Destination dependency : destination.getDependencies()) {
+                order(dependency);
+            }
+        }
+        destinationNames.add(0, destination.getName());
     }
 
     public String getName() {
