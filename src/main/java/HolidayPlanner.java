5e20947d2a6e013e044a3654bdde1cda0b16219e
eszterkaloz <eszterkaloz@gmail.com>
2017-03-29 21:57:43 +0200

    Handle possible cycles in the graph

--- src/main/java/HolidayPlanner.java
+++ src/main/java/HolidayPlanner.java
@@ -4,24 +4,29 @@ import java.util.List;
 public class HolidayPlanner {
     private List<String> destinationNames = new ArrayList<>();
 
-    public List<String> planDestination(Destination... destinations) {
-        for(Destination destination : destinations) {
+    public List<String> planDestination(Destination... destinations) throws Exception {
+        for (Destination destination : destinations) {
             order(destination);
         }
         return destinationNames;
     }
 
-    private void order(Destination destination) {
-        if (destination.isVisited()) {
+    private void order(Destination destination) throws Exception {
+        if (destinationNames.contains(destination.getName())) {
             return;
         }
 
+        if (destination.isVisited()) {
+            throw new Exception("Not a DAG");
+        }
+
         destination.setVisited(true);
-        if(destination.getDependencies() != null) {
+        if (destination.getDependencies() != null) {
             for (Destination dependency : destination.getDependencies()) {
                 order(dependency);
             }
         }
+
         destinationNames.add(0, destination.getName());
     }
 }
